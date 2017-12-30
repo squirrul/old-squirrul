56371e3ccb256f3d227198bbfb3dd3087ec78c2f
John Rees <johnrees@users.noreply.github.com>
2017-12-30 13:56:12 +0000

    gundb working with (p)react

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
-
+import Gun from 'gun/gun';
 import Header from './header';
 import Home from '../routes/home';
 import Profile from '../routes/profile';
@@ -8,6 +8,14 @@ import Profile from '../routes/profile';
 // import Profile from 'async!../routes/profile';
 
 export default class App extends Component {
+
+	constructor() {
+		super()
+		this.gun = Gun('https://gunjs.herokuapp.com/gun').get('random/fDO4CKq98');
+		// this.gun = Gun([location.origin, 'gun'].join('/'))
+		window.gun = this.gun;
+	}
+
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
@@ -21,7 +29,7 @@ export default class App extends Component {
 			<div id="app">
 				<Header />
 				<Router onChange={this.handleRoute}>
-					<Home path="/" />
+					<Home path="/" gun={this.gun} />
 					<Profile path="/profile/" user="me" />
 					<Profile path="/profile/:user" />
 				</Router>
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,12 +1,55 @@
+import Gun from "gun/gun";
 import { h, Component } from 'preact';
 import style from './style';
 
+const formatNotes = notes => Object.keys(notes)
+	.map(key => ({ key, val: notes[key] }))
+	.filter(t => Boolean(t.val) && t.key !== '_')
+
 export default class Home extends Component {
-	render() {
+
+	state = {
+		newNote: '',
+		notes: []
+	}
+
+	constructor(props) {
+		super(props)
+		this.gun = this.props.gun.get('notes')
+	}
+
+	componentWillMount() {
+		this.gun.on(notes => this.setState({
+			notes: formatNotes(notes)
+		}))
+	}
+
+	add = e => {
+		e.preventDefault();
+		this.gun.get(Gun.text.random()).put(this.state.newNote)
+		this.setState({newNote: ''})
+	}
+
+	remove = key => e => {
+		e.preventDefault();
+		this.gun.get(key).put(null)
+	}
+
+	handleChange = e => this.setState({newNote: e.target.value})
+
+	render(_, {newNote, notes}) {
+
 		return (
 			<div class={style.home}>
 				<h1>Home</h1>
 				<p>This is the Home component.</p>
+				<form onSubmit={this.add}>
+					<input value={newNote} onChange={this.handleChange} />
+					<button onClick={this.add}>Add</button>
+				</form>
+				<ul>
+				{notes.map(note => <li key={note.key} onClick={this.remove(note.key)}>{note.val}</li>)}
+				</ul>
 			</div>
 		);
 	}
